48e7508f293d939499c6214eef04da633bec2cef
adrian-clarck <adrian.clarck@aluno.senai.br>
2026-03-12 07:27:02 -0300

    Desenvolvimento em mobile, aprendendo mais com o Flex

--- MOBILE/REACT-NATIVE/first-app/src/App.js
+++ MOBILE/REACT-NATIVE/first-app/src/App.js
@@ -1,18 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Exemplo01 from './jsx_examples/exemplo-01-estrutura';
-import Exemplo02 from './jsx_examples/exemplo-02-expressoes';
-import Exemplo03 from './jsx_examples/exemplo-03-condicionais';
-import Exemplo04 from './jsx_examples/exemplo-04-listas';
-import Lista01 from './lista_exercicios/lista-01/adrian-clarck-lista01';
-import Lista02 from './lista_exercicios/lista-02/adrian-clarck-lista02';
-import Lista03 from './lista_exercicios/lista-03/adrian-clarck-lista03';
+import ViewExample from './basic_components/view_examples01';
+import ViewExample02 from './basic_components/view_examples02';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Lista03 />
+      <ViewExample02 />
     </View>
   );
 }
