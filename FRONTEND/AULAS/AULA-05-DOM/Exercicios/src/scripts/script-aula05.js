f958978e68504d6226f6585d3bd896108c2d73e4
adrian-clarck <adrian.clarck@aluno.senai.br>
2026-03-19 12:47:35 -0300

    Criação e término de atividade sobre DOM Estático, desenvolvimento do script em relação ao HTML

---
+++ FRONTEND/AULAS/AULA-05-DOM/Exercicios/src/scripts/script-aula05.js
@@ -0,0 +1,21 @@
+// 1. Identidade: alterar o nome do usuário
+const nomeUsuario = document.querySelector("#nome-usuario");
+nomeUsuario.textContent = "Adrian Clarck";
+
+// 2. Avatar: trocar a imagem de perfil
+const fotoPerfil = document.querySelector("#foto-perfil");
+fotoPerfil.src =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOq762mu2M0UTVJ8fQmHGtBqBWpACHbb03vQ&s";
+
+// 3. Personalização: mudar cor de fundo do container
+const container = document.querySelector("#container-perfil");
+container.style.backgroundColor = "#e3f2fd";
+
+// 4. Status Real: adicionar classe e mudar texto
+const status = document.querySelector("#badge-status");
+status.classList.add("online");
+status.textContent = "Status: Ativo";
+
+// 5. Auditoria: contar skills e mostrar no console
+const skills = document.querySelectorAll(".skill");
+console.log("Total de skills:", skills.length);
