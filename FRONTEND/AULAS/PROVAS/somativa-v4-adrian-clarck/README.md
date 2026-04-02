51f2536dd0cc29b347f8e4c32c46eabd0be5e7ca
adrian-clarck <adrian.clarck@aluno.senai.br>
2026-04-02 14:35:08 -0300

    Realização da avaliação Somativa 01, com inserções de script, DOM e Eventos DOM, feito tambem o README descrevendo o projeto.

---
+++ FRONTEND/AULAS/PROVAS/somativa-v4-adrian-clarck/README.md
@@ -0,0 +1,30 @@
+# 💻 Site CinePipoca 
+
+Este é um projeto realizado para uma avaliação de front-end, focada na inserção de scripts, DOM e Eventos DOM, e demonstra um site para um Cinema.
+
+## 🛠️ Tecnologias Utilizadas
+* HTML5
+* CSS
+* JavaScript
+
+## 📁 Estrutura do Projeto
+Organização do Projeto::
+/
+├── index.html
+└── src/
+    ├── styles/  
+        └── style.css
+    ├── scripts/ 
+        └── script.js
+    └── images/  
+        └── pipoca.png
+
+## 🚀 Funcionalidades Implementadas
+* **Interatividade Visual:** Foi implementado um evento de entrada e saída de mouse para o Banner Vip, quando o mouse entra no banner ativa o modo vip e quando sai o modo vip sai junto.
+* **Saudação Dinâmica:** Uma mensagem que altera de acordo com a hora do usuário, dizendo que libera uma das 3 sessões especificas (Manhã, Tarde, Noite).
+* **Cálculo em Tempo Real:** Na compra de bilhetes foi implementado um evento de input que calcula em tempo real o preço da compra de ingressos, dependendo do valor de ingressos que serão comprados, que é informado pelo usuário.
+* **Injeção de Conteúdo:** Foram adicionados dois eventos de Clique, aonde o primeiro, com o botão de Publicar Filme, insere na tela a informação escrita pelo usuário, e o segundo, o botão de Limpar Mural, que apaga a informação inserida anteriormente e o retorna para a caixa de escrita.
+
+## 👨‍💻 Autor
+Adrian Clarck
+GitHub: adrian-clarck
---
+++ FRONTEND/AULAS/PROVAS/somativa-v4-adrian-clarck/src/scripts/script.js
@@ -0,0 +1,46 @@
+// Missão 01
+const saudacao = document.querySelector("#saudacao-cinema")
+const hora = new Date().getHours()
+if (saudacao) {
+    saudacao.textContent =
+        hora < 12 
+            ? "Bom dia! Bem-vindo à Sessão Matinê!"
+            : hora >= 12 || hora <= 18 
+                ? "Boa tarde! Sessão da Tarde liberada!"
+                : "Boa noite! Prepare-se para a Sessão Coruja!"
+}
+
+// Missão 02
+const bannerInject = document.querySelector("#banner-vip")
+bannerInject.addEventListener("mouseenter", () => {
+    bannerInject.classList.add("modo-vip")
+})
+bannerInject.addEventListener("mouseleave", () => {
+    bannerInject.classList.remove("modo-vip")
+})
+
+// Missão 03
+const bilheteria = document.querySelector("#qtd-ingressos")
+const totalPagar = document.querySelector("#total-pagar")
+bilheteria.addEventListener("input", () => {
+    const precoIngresso = 35.0;
+    const total = Number(bilheteria.value) * precoIngresso;
+    totalPagar.textContent = `R$ ${total.toFixed(2)}`;
+});
+
+// Missão 04
+const publicarFilme = document.querySelector("#btn-publicar")
+const nomeFilme = document.querySelector("#nome-filme")
+const muralCritica = document.querySelector("#mural-criticas")
+
+publicarFilme.addEventListener("click", (event) => {
+    muralCritica.innerHTML = (`<h3>🎬 Filme: ${nomeFilme.value.trim()} ⭐</h3>`)
+})
+
+// Missão 05
+const limparMural = document.querySelector("#btn-apagar")
+
+limparMural.addEventListener("click", (event) => {
+    muralCritica.innerHTML = ("")
+    nomeFilme.focus()
+})
